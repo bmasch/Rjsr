a4d8a3408237fb843916ad779f9744a5da0c79d5
bmasc <bmasch@gmail.com>
2024-03-06 20:38:45 -0800

    Build Reports dataframe

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Links to Joint Staff Reports
+#'
+#' Data frame of current and past Joint Staff Reports
+#' Report ...
+#'
+#' @format ## `Reports`
+#' A data frame with 4 columns:
+#' \describe{
+#'   \item{year}{Year report generated}
+#'   \item{report}{Spring or Fall}
+#'   \item{title}{Report title}
+#'   \item{href}{Report URL}
+#' }
+#' @source <https://wdfw.wa.gov/fishing/management/columbia-river/compact/other-information>
+"Reports"
--- data-raw/buildData.R
+++ data-raw/buildData.R
@@ -2,6 +2,7 @@ library(pdftools)
 library(dplyr)
 library(stringr)
 library(jsonlite)
+library(rvest)
 
 #set the data-raw folder as working directory, then run code below
 
@@ -25,4 +26,40 @@ usethis::use_data(Current.Report, overwrite = TRUE)
 Table.Config <- fromJSON("table.config.json")
 
 usethis::use_data(Table.Config, overwrite = TRUE)
+
+
+
+html <- read_html("https://wdfw.wa.gov/fishing/management/columbia-river/compact/other-information")
+
+p.elements <- html %>% html_elements("p")
+
+Reports <- data.frame()
+
+for(i in 1:length(p.elements)){
+  p.text <- (html_text(p.elements[i]))
+  if(grepl("Joint Staff Report:",p.text)){
+    if(grepl("pring Chinook",p.text)){
+      a.element <- html_elements(p.elements[i],"a")
+      href <- html_attr(a.element,"href")
+      if(substring(href,1,1)=="/")href <- paste0("https://wdfw.wa.gov",href)
+      textlines <- p.text %>% strsplit(split="\n")
+      Reports <- rbind(Reports,data.frame(report="Spring",title=textlines[[1]][2],href=href))
+    }
+    if(grepl("all Chinook Salmon",p.text)){
+      a.element <- html_elements(p.elements[i],"a")
+      href <- html_attr(a.element,"href")
+      if(substring(href,1,1)=="/")href <- paste0("https://wdfw.wa.gov",href)
+      textlines <- p.text %>% strsplit(split="\n")
+      Reports <- rbind(Reports,data.frame(report="Fall",title=textlines[[1]][2],href=href))
+    }
+  }
+}
+
+Reports <- Reports %>%
+  mutate(year= as.numeric(substring(title,1,4))) %>%
+  select(year,report,title,href)
+
+usethis::use_data(Reports, overwrite = TRUE)
+
+
 
